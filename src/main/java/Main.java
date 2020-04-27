4ba97f03467b1440421cf1b4675332ae42cd0459
springboot-mymzt <2122877473@qq.com>
2020-04-28 00:44:34 +0800

    第一次提交

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.apache.commons.lang3.StringUtils;
+
+
+public class Main {
+
+
+    public static void main(String[] args){
+        String string = "ajjjfg";
+        if(StringUtils.isNotBlank(string)){
+            System.out.println("非空字符串");
+        }
+    }
+}
